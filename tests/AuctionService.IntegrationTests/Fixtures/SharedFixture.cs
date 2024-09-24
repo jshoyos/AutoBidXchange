d25018aa4eaa06056f4adbde13be1463e5abe03e
Juan Hoyos <juanse.jsh@gmail.com>
2024-09-24 00:13:20 -0400

    fix docker files and reuse fixture

---
+++ tests/AuctionService.IntegrationTests/Fixtures/SharedFixture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionService.IntegrationTests.Fixtures
+{
+    [CollectionDefinition("SharedCollection")]
+    public class SharedFixture : ICollectionFixture<CustomWebAppFactory>
+    {
+    }
+}
