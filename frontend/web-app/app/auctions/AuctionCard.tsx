1df0c892f23313e2aa70821b2f1a78896e040d0b
Hokage <juanse.jsh@gmail.com>
2024-11-23 18:03:37 -0500

    fixed typo

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import CountdownTimer from './CountDownTimer'
+import CountdownTimer from './CountdownTimer'
 
 type Props = {
    auction: any
